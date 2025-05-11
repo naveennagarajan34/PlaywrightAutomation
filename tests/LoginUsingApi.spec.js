bda2490f12057c3b7d968464d6d3cef13f7fd634
Naveen N <naveennagarajan34@gmail.com>
2025-05-11 23:06:53 +0530

    login using web api via token

---
+++ tests/LoginUsingApi.spec.js
@@ -0,0 +1,27 @@
+const { test, expect, request } = require("@playwright/test");
+const loginPayLoad = {
+  userEmail: "naveennagarajan34@gmail.com",
+  userPassword: "Naveen@123",
+};
+
+let token;
+test.beforeAll(async () => {
+  const apiContext = await request.newContext();
+  const loginResponse = await apiContext.post(
+    "https://rahulshettyacademy.com/api/ecom/auth/login",
+    {
+      data: loginPayLoad,
+    }
+  );
+  expect(loginResponse.ok()).toBeTruthy(); // Optional, but useful
+  const loginResponseJson = await loginResponse.json(); // ✅ await added
+  token = loginResponseJson.token;
+});
+
+test.only("Login Using Api token", async ({ page }) => {
+  page.addInitScript((value) => {
+    window.localStorage.setItem("token", value);
+  }, token);
+  await page.goto("https://rahulshettyacademy.com/client");
+  page.pause();
+});
