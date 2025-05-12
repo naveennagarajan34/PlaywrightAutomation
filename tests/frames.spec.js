7f845f3dcb9f2dc94a5d7cd1d7c639d83d4ea3da
Naveen N <naveennagarajan34@gmail.com>
2025-05-12 22:20:22 +0530

    frames handling

--- tests/frames.spec.js
+++ tests/frames.spec.js
@@ -1,6 +1,6 @@
 const { test, expect } = require("@playwright/test");
 
-test.only("Pop up", async ({ page }) => {
+test.only("Pop up and Frames", async ({ page }) => {
   await page.goto("https://rahulshettyacademy.com/AutomationPractice/");
   expect(await page.locator("#displayed-text")).toBeVisible();
   await page.locator("#hide-textbox").click();
@@ -8,5 +8,9 @@ test.only("Pop up", async ({ page }) => {
   await page.locator("#confirmbtn").click();
   await page.on("dialog", (dialog) => dialog.accept());
   await page.locator("#mousehover").hover();
+  const framePage = await page.frameLocator("#courses-iframe");
+  expect(
+    await framePage.locator("ul.navigation li a[href*='courses']").first()
+  ).toBeVisible();
   await page.waitForTimeout(10000);
 });
